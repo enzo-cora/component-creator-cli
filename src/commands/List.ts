dc9739670fcca7b70278aba5870d5eb627810952
enzo <enzocoratella13@gmail.com>
2021-11-26 11:15:48 +0100

    change table dependency

--- src/commands/List.ts
+++ src/commands/List.ts
@@ -1,5 +1,6 @@
+/*
 import {TemplateRepo} from "../TemplateRepository";
-import {cInfo, cSuccess} from "../_constantes/config";
+import {cSuccess} from "../_constantes/config";
 import {getBorderCharacters, table} from "table";
 import chalk from "chalk";
 import {InfoMsgs} from "../_constantes/InfoMsgs";
@@ -52,6 +53,58 @@ export const list : IListCommand = async ()=>
   else
     console.log(table([[InfoMsgs.NO_TEMPLATE]]))
 
+
+}
+
+
+*/
+
+import Table from "cli-table";
+
+import {TemplateRepo} from "../TemplateRepository";
+import {cSuccess} from "../_constantes/config";
+import chalk from "chalk";
+import {InfoMsgs} from "../_constantes/InfoMsgs";
+
+
+export type IListCommand = () => Promise<void>
+type ITableContent = Array<[TemplateName:string, WorkDire:string, ExentionWrkDir:string]>
+
+
+export const list : IListCommand = async ()=>
+{
+  const tempMetaDataResult = await TemplateRepo.getAllMetadata()
+  if(tempMetaDataResult instanceof Error)
+    return console.log(tempMetaDataResult.message)
+
+  if(tempMetaDataResult.length === 0){
+    const table = new Table({ colAligns : ["middle"]})
+    table.push([InfoMsgs.NO_TEMPLATE])
+    return console.log(table.toString())
+  }
+
+  const table   : ITableContent = new Table({
+    colAligns: ["middle","middle","middle"],
+  })
+
+  tempMetaDataResult.forEach(
+    ({configFile}) => {
+      const tempName :string = configFile.template
+      let wrkDirectory :string
+      let extensionDirectory :string = "none"
+      if(typeof configFile.componentWorkDir === "string")
+        wrkDirectory = configFile.componentWorkDir
+      else{
+        wrkDirectory = configFile.componentWorkDir.rootWorkDir
+        if(configFile.componentWorkDir.extensionWorkDir)
+          extensionDirectory = configFile.componentWorkDir.extensionWorkDir
+      }
+
+      table.push([chalk.blue(tempName),cSuccess(wrkDirectory),cSuccess(extensionDirectory)])
+    })
+
+  table.unshift(    ["Name","Working Directory","Extension Working Directory"])
+  return console.log(table.toString())
 
 }
 
